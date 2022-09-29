b2e4bf62393f53be84a78c4933214eee9c21c641
Paul-hengChen <fgvb88997@gmail.com>
2022-09-30 00:18:34 +0800

    feat: create basic Chart

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './style/app.scss';
 import './style/tailwindcss.css';
 import 'animate.css';
+import { Chart } from './component';
 
 const App = () => (
-  <div className="App" />
+  <div className="App">
+    <Chart />
+  </div>
 );
 
 export default App;
